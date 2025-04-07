aea610c63fd437e5cbcb2dc2d5a279fa42622f08
star88etti <nlevran@gmail.com>
2025-04-07 13:45:24 +0300

    Updates

--- src/components/FilterBar.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-// Empty FilterBar component since we're removing the category buttons
-const FilterBar: React.FC = () => {
-  return null;
-};
-
-export default FilterBar;
